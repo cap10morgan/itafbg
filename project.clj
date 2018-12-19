28fc4d2bcf7e837bc39538ee911fdf8b2db0389b
Wes Morgan <wesmorgan@icloud.com>
2018-12-19 13:27:26 -0700

    Update clojure to 1.10.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.mnode.ical4j/ical4j "1.0.2"]
                  [clj-http "3.9.1"]
                  [clj-time "0.15.1"]
