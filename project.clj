465dff4495d3a588e5e340c33b1d97efe770d4a4
Wes Morgan <wesmorgan@hey.com>
2021-03-21 18:50:41 -0600

    Update deps to latest versions

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/cap10morgan/itafbg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.mnode.ical4j/ical4j "3.0.11"]
-                 [clj-http "3.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.mnode.ical4j/ical4j "3.0.22"]
+                 [clj-http "3.12.1"]
                  [clj-time "0.15.2"]
-                 [compojure "1.6.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
-                 [environ "1.1.0"]
+                 [compojure "1.6.2"]
+                 [ring/ring-jetty-adapter "1.9.2"]
+                 [environ "1.2.0"]
                  [hiccup "1.0.5"]]
   :min-lein-version "2.7.1"
   :plugins [[environ/environ.lein "0.3.1"]]
