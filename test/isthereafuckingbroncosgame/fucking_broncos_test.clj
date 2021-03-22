044e1357f78e8488743393f3bbd7de8e71824e23
Wes Morgan <wesmorgan@hey.com>
2021-03-21 19:06:19 -0600

    Add a test for the most important fn in the whole fucking project

--- test/isthereafuckingbroncosgame/fucking_broncos_test.clj
+++ test/isthereafuckingbroncosgame/fucking_broncos_test.clj
@@ -8,3 +8,7 @@
     (is (not (is-it-fucking-football-season? (t/local-date 2021 03 21)))))
   (testing "October 5 is (sadly) fucking football season"
     (is (is-it-fucking-football-season? (t/local-date 2021 10 05)))))
+
+(deftest is-there-a-fucking-broncos-game?-test
+  (testing "There sure as fuck was a Broncos game on Jan. 3, 2021"
+    (is (is-there-a-fucking-broncos-game? (t/local-date 2021 1 3)))))
