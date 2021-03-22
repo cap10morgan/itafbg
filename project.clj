7fb240f8f0435822b9c2b58658f72fca3697f090
Wes Morgan <wesmorgan@hey.com>
2021-03-21 18:50:31 -0600

    Fill out the fucking desc & url fields in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject isthereafuckingbroncosgame "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Is there a fucking Broncos game?"
+  :url "https://github.com/cap10morgan/itafbg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
