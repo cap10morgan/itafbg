1f90d929df97675ddba07743190dd4fa22ed2e0b
Wes Morgan <git@wesmorgan.me>
2026-01-14 10:47:38 -0700

    Ensure today element retains HTML id

--- resources/js/today.js
+++ resources/js/today.js
@@ -24,9 +24,11 @@ const months = [
 ];
 
 function setTodayElement() {
-    var today = new Date();
-    var targetElement = document.getElementById("today-element");
-    var newElement = document.createElement("span");
+    const today = new Date();
+    const todayId = "today-element";
+    const targetElement = document.getElementById(todayId);
+    const newElement = document.createElement("span");
+    newElement.id = todayId;
 
     newElement.innerHTML = `Today is ${days[today.getDay()]}, ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
     targetElement.replaceWith(newElement);
